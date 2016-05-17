e38185ce19c78f8456836c9fdf78b2627896bc4d
Kathure <lizkathure@gmail.com>
2016-05-17 22:41:51 +0300

    added foundation

--- hello.py
+++ hello.py
@@ -1,12 +1,25 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 from flask.ext.script import Manager
+from flask.ext.moment import Moment
 
 app = Flask(__name__)
 
 manager = Manager(app)
+moment = Moment(app)
 
-@app.route('/index')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/')
 def index():
     return render_template('index.html')
 
@@ -15,15 +28,7 @@ def index():
 def user(name):
     return render_template('user.html', name=name)
 
+
 if __name__ == '__main__':
-   manager.run()
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'),404
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
+    manager.run()
 
